d15551f1f8021b91f31c6764ec1c07f86d5800d9
Romelius322 <a2tsokur@gmail.com>
2026-03-18 15:20:08 +0300

    Лабораторная работа 2

---
+++ laba2/task1
@@ -0,0 +1,14 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+months=0
+while money_capital + salary>=spend:
+    budget=money_capital+salary
+    budget-=spend
+    money_capital=budget
+    months+=1
+    spend*=(1+increase)
+# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
+
+print("Количество месяцев, которое можно протянуть без долгов:", months)
---
+++ laba2/task2.py
@@ -0,0 +1,13 @@
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+money_capital=0
+for m in range(months):
+    expenses = spend*(1+increase)**m
+    shortfall=expenses - salary
+    money_capital+=shortfall
+money_capital=round(money_capital)
+# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
+
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money_capital)
