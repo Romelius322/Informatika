edd227b0ed1bf982c7b2abd36870de3420a7b2ad
Romelius322 <a2tsokur@gmail.com>
2026-06-14 18:42:07 +0300

    task1.py

--- laba2/task1.py
+++ laba2/task1.py
@@ -2,13 +2,16 @@ money_capital = 20000  # Подушка безопасности
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
-months=0
-while money_capital + salary>=spend:
-    budget=money_capital+salary
-    budget-=spend
-    money_capital=budget
-    months+=1
-    spend*=(1+increase)
-# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
 
-print("Количество месяцев, которое можно протянуть без долгов:", months)
+# TODO Посчитайте количество  месяцев, которое можно протянуть без долго
+months = 0
+current_spend = spend  # траты в текущем месяце (первый месяц без повышения)
+
+while money_capital + salary >= current_spend:
+    # Тратим из подушки разницу между расходами и зарплатой
+    money_capital -= (current_spend - salary)
+    months += 1
+    # Увеличиваем расходы на следующий месяц
+    current_spend *= (1 + increase)
+
+print("Количество месяцев, которое можно протянуть без долгов:", months )
