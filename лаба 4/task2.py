aed33af171c0fee85456f86fbfa84d499bc55bf8
Romelius322 <a2tsokur@gmail.com>
2026-06-14 18:49:13 +0300

    task2.py

--- лаба 4/task2.py
+++ лаба 4/task2.py
@@ -1,36 +1,15 @@
-# TODO импортировать необходимые молули
+# TODO решите задачу
 import json
+def task() -> float:
+    # Открываем файл
+    with open('input.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
 
-INPUT_FILENAME = "input.csv"
-OUTPUT_FILENAME = "output.json"
+    total = 0.0
+    for item in data:
+        total += item['score'] * item['weight']
 
-
-def task() -> None:
-    with open(INPUT_FILENAME, 'r', encoding='utf-8') as file:
-        lines = file.readlines()
-    headers = lines[0].strip().split(',')
-
-    result = []
-    for i in range(1, len(lines)):
-        if lines[i].strip() == "":
-            continue
-
-        values = lines[i].strip().split(',')
-        row_dict = {}
-        for j in range(len(headers)):
-            row_dict[headers[j]] = values[j]
-
-        result.append(row_dict)
-    ...  # TODO считать содержимое csv файла
-    with open(OUTPUT_FILENAME, 'w', encoding='utf-8') as f:
-        json.dump(result, f, indent=4, ensure_ascii=False)
-    # TODO Сериализовать в файл с отступами равными 4
+    return round(total, 3)
 
 
-if __name__ == '__main__':
-    # Нужно для проверки
-    task()
-
-    with open(OUTPUT_FILENAME) as output_f:
-        for line in output_f:
-            print(line, end="")
+print(task())
