8d8fe04fbeb54bbe89e4c52433a3e1591559be37
Romelius322 <a2tsokur@gmail.com>
2026-06-14 18:44:30 +0300

    task1.py

--- лаба 1/task1.py
+++ лаба 1/task1.py
@@ -1,13 +1,27 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # TODO заменить значение пропущенного элемента средним арифметическим
-none_index = numbers.index(None)
-total=0
+# Находим индекс пропущенного элемента (где значение None)
+index_none = None
 for i in range(len(numbers)):
-    if numbers[i] is not None:
-        total=total + numbers[i]
-count = len(numbers)
-average=total/count
-numbers[none_index]=average
-print("Измененный список:", numbers)
+    if numbers[i] is None:
+        index_none = i
+        break
 
+# Считаем сумму всех чисел, кроме пропуска
+summa = 0
+for i in range(len(numbers)):
+    if numbers[i] is not None:  # пропускаем None
+        summa += numbers[i]
+
+# Считаем количество элементов (включая пропуск)
+kolichestvo = len(numbers)
+
+# Вычисляем среднее арифметическое
+# Формула: сумма всех чисел / общее количество элементов
+srednee = summa / kolichestvo
+
+# Заменяем пропущенный элемент на среднее арифметическое
+numbers[index_none] = srednee
+
+print("Измененный список:", numbers)
